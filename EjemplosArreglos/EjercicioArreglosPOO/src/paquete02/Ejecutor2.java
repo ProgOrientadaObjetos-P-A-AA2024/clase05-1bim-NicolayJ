3fa34f3021d84e8c8455810244580bff12132e3a
NicolayJ <dnjimenez6@utpl.edu.ec>
2024-05-15 16:11:13 -0500

    Listo =D

--- EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
@@ -17,12 +17,18 @@ public class Ejecutor2 {
         
         
         
-        LibretaCalificacion libreta2 = new LibretaCalificacion(nombreEstudiante,
+        LibretaCalificacion libreta2 = new LibretaCalificacion(
+                nombreEstudiante,
                 notas);
         
         libreta2.establecerPromedio();
+        // Si comento la linea 24 lo que pasaria es que no se llamaria al metodo 
+        // establecerPromedio y por ende tomaria el valor por defecto al imprimirlo
+        // (0,00 porque es el valor por defecto de una variable de tipo double)
         libreta2.establecerPromedioCualitativo();
-        
+        // Si comento la linea 28 lo que pasaria es que no se llamaria al metodo 
+        // establecerPromedioCualitativo y por ende tomaria el valor por defecto 
+        //al imprimirlo (null porque es el valor por defecto de una cadena vacia)
         System.out.printf("%s",libreta2);
     }
 }
